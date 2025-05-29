f10cebe45d4ea99ad69dd65b790fab1ebe1c5cfa
HugoNPB <2254858@cegepmontpetit.ca>
2025-05-29 17:41:31 -0400

    Plateforme eCommerce

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,12 +1,40 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DbSingletonService, Product} from "../dbSingleton.service";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private dbSingleton: DbSingletonService) {
+  }
+
+  ngOnInit(){
+    this.products = this.dbSingleton.products;
+  }
+
+  get subtotal(): number {
+    return this.products.reduce((sum, item) => sum + item.price, 0);
+  }
+
+  get tax(): number {
+    return this.subtotal * 0.2; // 15% tax
+  }
+
+  get total(): number {
+    return this.subtotal + this.tax;
+  }
+
+  payNow() {
+    alert('Lorem!');
+  }
 }
--- src/app/dbSingleton.service.ts
+++ src/app/dbSingleton.service.ts
@@ -10,61 +10,71 @@ export class DbSingletonService {
       name: 'Produit 1',
       imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Google_Chrome_icon_%28February_2022%29.svg/1200px-Google_Chrome_icon_%28February_2022%29.svg.png',
       price: 1,
-      OnSale: false
+      onSale: false,
+      onCart: true
     },
     {
       name: 'Produit 2',
       imageUrl: 'https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/image9-2.jpg?width=432&name=image9-2.jpg',
       price: 2,
-      OnSale: false
+      onSale: false,
+      onCart: false
     },
     {
       name: 'Produit 3',
       imageUrl: 'https://cdn.futura-sciences.com/buildsv6/images/largeoriginal/5/e/6/5e6f4ecb84_100517_google-definition.jpg',
       price: 3,
-      OnSale: false
+      onSale: false,
+      onCart: false
     },
     {
       name: 'Téléphone intelligent XYZ',
       imageUrl: 'https://images.unsplash.com/photo-1598327105666-5b89351aff97?q=80&w=1000&auto=format&fit=crop',
       price: 499,
-      OnSale: true
+      onSale: true,
+      onCart: false
     },
     {
       name: 'Portables',
       imageUrl: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=1000&auto=format&fit=crop',
       price: 899,
-      OnSale: false
+      onSale: false,
+      onCart: false
     },
     {
       name: 'Écouteurs Sans-Fil',
       imageUrl: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=1000&auto=format&fit=crop',
       price: 129,
-      OnSale: false
+      onSale: false,
+      onCart: false
     },
     {
       name: 'Montre intelligente',
       imageUrl: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?q=80&w=1000&auto=format&fit=crop',
       price: 199,
-      OnSale: false
+      onSale: false,
+      onCart: true
     },
     {
       name: 'Haut-parleur Bluetooth',
       imageUrl: 'https://images.unsplash.com/photo-1608043152269-423dbba4e7e1?q=80&w=1000&auto=format&fit=crop',
       price: 79,
-      OnSale: false
+      onSale: false,
+      onCart: false
     },
     {
       name: 'Camera Digital',
       imageUrl: 'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?q=80&w=1000&auto=format&fit=crop',
       price: 349,
-      OnSale: true
+      onSale: true,
+      onCart: false
     },
     {
       name: 'Console de jeu',
       imageUrl: 'https://images.unsplash.com/photo-1486572788966-cfd3df1f5b42?q=80&w=1000&auto=format&fit=crop',
       price: 299,
-      OnSale: false
+      onSale: false,
+      onCart: false
     },
   ];
 
@@ -101,7 +111,8 @@ export interface Product {
   name: string;
   imageUrl: string;
   price: number;
-  OnSale: boolean;
+  onSale: boolean;
+  onCart: boolean;
 }
 
 export interface Employee {
